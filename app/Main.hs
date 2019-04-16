45b71dff16ba79bc9655596f21e0ae0b87bc4f64
wurthel <salmanov.vh@gmail.com>
2019-04-16 22:08:52 +0300

    Revert "Update"
    This reverts commit 3ce7193ed435e11badbe3117ee7615339a41c679.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import InsertMolecule
-import Data.Maybe (fromMaybe)
 import System.Environment
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
 main = do insertMolecule
@@ -10,16 +10,15 @@ main = do insertMolecule
 insertMolecule :: IO ()
 insertMolecule = do
     args <- getArgs
-    -- let zmatr_name = args !! 0
-    --     mol_name   = args !! 1
-    --     n = read $ args !! 2
-    --     s = read $ args !! 3
-    --     e = read $ args !! 4
-    --     res_file = args !! 5
-    print (unwords args)
-    -- zmatrix    <- readZMatrix zmatr_name
-    -- molecule   <- readMolecule mol_name
-    -- let molecule' = fromMaybe (error "insertMolecule: returned Nothing") 
-    --                 $ setAtomWithOutOptimization n zmatrix molecule
-    -- molecule'' <- setAtomWithOptimization s e zmatrix molecule'
-    -- writeMolecule res_file molecule''
+    let zmatr_name = args !! 0
+        mol_name   = args !! 1
+        n = read $ args !! 2
+        s = read $ args !! 3
+        e = read $ args !! 4
+        res_file = args !! 5
+    zmatrix    <- readZMatrix zmatr_name
+    molecule   <- readMolecule mol_name
+    let molecule' = fromMaybe (error "insertMolecule: returned Nothing") 
+                    $ setAtomWithOutOptimization n zmatrix molecule
+    molecule'' <- setAtomWithOptimization s e zmatrix molecule'
+    writeMolecule res_file molecule''
--- src/Config.hs
+++ src/Config.hs
@@ -1,6 +1,5 @@
 module Config where
 
-import System.Environment
 import System.IO.Unsafe
 import Data.List.Split
 import Data.List
@@ -21,30 +20,13 @@ get key f = case lookup key readConfig of
     Nothing -> error ("get: not found: " ++ key)
     Just x  -> f x
 
--- Имя файла структуры со структурой ZMatrix
-zmatrix_fn :: String
-zmatrix_fn = get "zmatrix" id 
-
--- Имя файла молекулы в формате pdb
-molecule_fn :: String
-molecule_fn = get "molecule" id
-
--- Число атомов, которое мы вставляем без оптимизации
-n_without_opt :: Int
-n_without_opt = get "n_without_opt" read
-
--- Диапазон атомов, которое мы вставляем с оптимизацией
-n_with_opt :: (Int, Int)
-n_with_opt = get "n_with_opt" read 
-
 -- | Файл для чтения белка после процесса оптимизации
-tmp_mol_inf :: String
-tmp_mol_inf = get "molecule_opt_aft" id
+mol_inf :: String
+mol_inf = get "mol_inf" id
 
--- | Имя файла оптимизируемой молекулы
-tmp_mol_ouf :: String
-tmp_mol_ouf = get "molecule_opt_beh" id
+-- | Файл для вывода белка перед процессом оптимизации
+mol_ouf :: String
+mol_ouf = get "mol_ouf" id
 
--- | Имя файла оптимизируемой молекулы (resid)
-tmp_resid_ouf :: String
-tmp_resid_ouf = get "resid_opt" id
+resid_ouf :: String
+resid_ouf = get "resid_ouf" id
